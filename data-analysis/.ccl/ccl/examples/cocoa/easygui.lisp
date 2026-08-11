516f4a4b3be7cf1cdcd6fd4dbb4579eb3297c9d2
Paul Emptoz <paul.emptoz@telecom-sudparis.eu>
2026-08-11 14:32:35 +0200

    Stop tracking files now covered by .gitignore

--- data-analysis/.ccl/ccl/examples/cocoa/easygui.lisp
+++
@@ -1,5 +0,0 @@
-(in-package :ccl)
-
-(let ((path (or *load-pathname* *loading-file-source-file*)))
-  (load (merge-pathnames ";easygui;system.lisp" path))
-  (load-easygui nil))
--- data-analysis/.mreasoner/mReasoner-2587fda/README.md
+++
@@ -1,2 +0,0 @@
-# mreasoner
-mReasoner is a unified computational implementation of the model theory of thinking and reasoning
